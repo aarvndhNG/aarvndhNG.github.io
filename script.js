27182814f6f774785b1cc5905c6dc555269e97ac
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2023-11-28 17:33:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,324 +1,86 @@
-// Function to generate a unique ID for each painting
-function generatePaintingID() {
-  return '_' + Math.random().toString(36).substr(2, 9);
-}
-
-// Function to filter paintings by ID
-function filterPaintingsByID(id) {
-  const paintings = document.querySelectorAll('.painting');
-
-  paintings.forEach(painting => {
-    const paintingID = painting.dataset.id;
-
-    if (id === '' || paintingID === id) {
-      painting.style.display = 'block'; // Show the painting
-    } else {
-      painting.style.display = 'none'; // Hide the painting
-    }
-  });
-}
-
-// Function to filter paintings by title
-function filterPaintingsByTitle(title) {
-  const paintings = document.querySelectorAll('.painting');
-
-  paintings.forEach(painting => {
-    const paintingTitle = painting.querySelector('h2').textContent.toLowerCase();
-
-    if (paintingTitle.includes(title)) {
-      painting.style.display = 'block'; // Show the painting
-    } else {
-      painting.style.display = 'none'; // Hide the painting
-    }
-  });
-}
-
-// Example painting data
-const paintings = [
-  {
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Cyberpunk Styled Spiderman',
-    image: 'inputs/Cyberpunk Style Spiderman (1).jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-  {
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Cyberpunk Styled Unknown Superhero',
-    image: '/inputs/Cyberpunk Style SuperHero.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-  {
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Bike Styled In Cyberpunk',
-    image: '/inputs/Cyberpunk bike 2.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-  {
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Bike Styled In Cyberpunk',
-    image: '/inputs/Cyberpunk bike.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Beautiful Princess With 🌍',
-    image: '/inputs/a universal princess.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Superhero With A Power Of Rainbow 🌈',
-    image: '/inputs/a unknown rainbow superhero.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Unknown Superhero With His Light And Fire Power',
-    image: '/inputs/a unknown superhero 2.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Super Of All Mighty',
-    image: '/inputs/a unknown superhero.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Cute Kitten (one)',
-    image: '/inputs/cute kitten 1.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Cute Kitten (two)',
-    image: '/inputs/cute kitten 2.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A cute sparkling kitten',
-    image: '/inputs/cute kitten Univers.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Cute Kitten (three)',
-    image: '/inputs/cute kitten.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Bike But In Cyberpunk Style',
-    image: '/inputs/Cyberpunk bike.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Car But In Cyberpunk Style',
-    image: '/inputs/Cyberpunk car.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Cyberpunk Samurai',
-    image: '/inputs/Cyberpunk samurai.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Cyberpunk Demon',
-    image: '/inputs/demon cyberpunk (1).jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Batman (one)',
-    image: '/inputs/devil bat man 2.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Evil Batman',
-    image: '/inputs/devil bat man.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Ironman',
-    image: '/inputs/evil Ironman .jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Ironman In digital painting',
-    image: '/inputs/Ironman cyberpunk.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Cyberpunk Samurai With Water Blade',
-    image: '/inputs/samurai cyberpunk.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'Ironman In Digital painting',
-    image: '/inputs/Spiderman cyberpunk.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Super Pet',
-    image: '/inputs/super pet .jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-{
-    id: generatePaintingID(), // Generate a unique ID for each painting
-    title: 'A Cyberpunk Superhero Cat',
-    image: '/inputs/superhero cat in Cyberpunk 2.jpg',
-    category: 'landscape',
-    date: '22-06-2023',
-    description: 'generator by AI you can request for this image in my Instagram by clicking the request button below '
-  },
-  
-  // Add more paintings as needed
-];
-
-const paintingsContainer = document.getElementById('paintings');
-const filterSelect = document.getElementById('filter');
-const sortSelect = document.getElementById('sort');
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-
-// Function to filter paintings based on category
-function filterPaintings(category) {
-  return category === 'all' ? paintings : paintings.filter(painting => painting.category === category);
-}
-
-// Function to sort paintings based on sort option
-function sortPaintings(sortOption) {
-  const sortedPaintings = [...paintings];
-
-  if (sortOption === 'title') {
-    sortedPaintings.sort((a, b) => a.title.localeCompare(b.title));
-  } else if (sortOption === 'date') {
-    sortedPaintings.sort((a, b) => new Date(a.date) - new Date(b.date));
-  }
-
-  return sortedPaintings;
-}
-
-// Function to render paintings on the page
-function renderPaintings(paintingsData) {
-  paintingsContainer.innerHTML = '';
-
-  paintingsData.forEach(painting => {
-    const paintingElement = document.createElement('div');
-    paintingElement.classList.add('painting');
-    paintingElement.dataset.id = painting.id; // Set the painting ID as a data attribute
-
-    const imageElement = document.createElement('img');
-    imageElement.src = painting.image;
-    imageElement.alt = painting.title;
-
-    const titleElement = document.createElement('h2');
-    titleElement.textContent = painting.title;
-
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = painting.description;
-
-    const timeElement = document.createElement('p');
-    timeElement.classList.add('upload-date');
-    timeElement.textContent = 'Recently uploaded';
-
-    const requestButton = document.createElement('button');
-    requestButton.classList.add('request-button');
-    requestButton.textContent = 'Request';
-    requestButton.dataset.instagram = 'https://www.instagram.com/aarvndh'; // Replace with your Instagram URL
-
-    paintingElement.appendChild(imageElement);
-    paintingElement.appendChild(titleElement);
-    paintingElement.appendChild(descriptionElement);
-    paintingElement.appendChild(timeElement);
-    paintingElement.appendChild(requestButton);
-
-    paintingsContainer.appendChild(paintingElement);
-  });
-
-  // Attach click event listener to each new request button
-  const newRequestButtons = document.querySelectorAll('.request-button');
-  newRequestButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const instagramURL = button.dataset.instagram;
-      window.open(instagramURL, '_blank');
+document.addEventListener('DOMContentLoaded', function () {
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
     });
-  });
+});
+
+function showDetails(artNumber) {
+    // Retrieve details for the specific art piece (replace with your data)
+    const details = getArtDetails(artNumber);
+
+    // Log details to console for demonstration (you can remove this line)
+    console.log(details);
+
+    // You can customize how you want to display the details, e.g., alert, console.log, etc.
+    alert(`
+    Art Title: ${details.title}
+    Positive Prompt: ${details.positivePrompt}
+    Negative Prompt: ${details.negativePrompt}
+    Seed: ${details.seed}
+    Steps: ${details.steps}
+    CFG Scale: ${details.cfgScale}
+    `);
 }
 
-// Initial rendering of paintings
-renderPaintings(paintings);
+// Replace this function with your data retrieval logic
+function getArtDetails(artNumber) {
+    // Set details for each artwork directly
+    if (artNumber === 1) {
+        return {
+            title: 'Art Title 1',
+            positivePrompt: 'm',
+            negativePrompt: 'Negative Prompt for Art 1',
+            seed: 'Seed for Art 1',
+            steps: 'Steps for Art 1',
+            cfgScale: 'CFG Scale for Art 1'
+        };
+    } else if (artNumber === 2) {
+        return {
+            title: 'Art Title 2',
+            positivePrompt: 'Positive Prompt for Art 2',
+            negativePrompt: 'Negative Prompt for Art 2',
+            seed: 'Seed for Art 2',
+            steps: 'Steps for Art 2',
+            cfgScale: 'CFG Scale for Art 2'
+        };
+    }
+    // Add more conditions for additional artworks
 
-// Event listeners for filter and sort options
-filterSelect.addEventListener('change', function () {
-  const category = this.value;
-  const filteredPaintings = filterPaintings(category);
-  renderPaintings(filteredPaintings);
-});
+    // Default case if artNumber doesn't match any condition
+    return {
+        title: 'Art Title Default',
+        positivePrompt: 'Positive Prompt Default',
+        negativePrompt: 'Negative Prompt Default',
+        seed: 'Seed Default',
+        steps: 'Steps Default',
+        cfgScale: 'CFG Scale Default'
+    };
+}
+function checkPassword() {
+    const enteredPassword = document.getElementById('password').value;
 
-sortSelect.addEventListener('change', function () {
-  const sortOption = this.value;
-  const sortedPaintings = sortPaintings(sortOption);
-  renderPaintings(sortedPaintings);
-});
+    // Replace 'yourPassword' with your actual password
+    if (enteredPassword === '1234') {
+        // Correct password, show the content
+        document.getElementById('password-container').style.display = 'none';
+        document.getElementById('gallery').style.display = 'block';
+    } else {
+        // Incorrect password, show an alert or handle it as needed
+        alert('Incorrect password. Please try again.');
+    }
+}
+// Existing JavaScript remains unchanged
 
-searchButton.addEventListener('click', () => {
-  const searchTitle = searchInput.value.trim().toLowerCase();
-  filterPaintingsByTitle(searchTitle);
-});
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const eyeIcon = document.querySelector('.eye-icon');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.innerHTML = '&#128064;'; // Closed eye icon
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.innerHTML = '&#128065;'; // Open eye icon
+    }
+}
